4f0979030fe6d5b58e1bb84ec5aa62dcdded6629
cold <ahuntdesign@gmail.com>
2011-11-16 21:07:00 -0500

    Got TraktClient working after wasting an hour trying it with my account and the WRONG password. Damnit all.

--- TraktClient.py
+++ TraktClient.py
@@ -2,12 +2,29 @@
 
 import simplejson as json
 import urllib
-import sha
+import logging
+
+from hashlib import sha1
 
 class TraktClient(object):
     def __init__(self, apikey, username, password):
-        password_hash = sha.new(password).hexdigest()
-        pass
+        self.username = username
+        self.password = sha1(password).hexdigest()
+        self.apikey = apikey
     
-    def callMethod(self, method):
-        pass
+    def call_method(self, method, data = {}, post=True):
+        method = method.replace("%API%", self.apikey)
+        
+        if (post):
+            data["username"] = self.username
+            data["password"] = self.password
+            
+            encoded_data = json.dumps(data);
+            
+            logging.debug(encoded_data)
+
+            stream = urllib.urlopen("http://api.trakt.tv/" + method,
+                                    encoded_data)
+            print stream.read()
+        else:
+            pass #Build url with data embedded.
